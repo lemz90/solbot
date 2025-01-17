6ae8f806fabd3032e6378c602f28acc1ca9be409
lemz90 <zrlemley@gmail.com>
2025-01-17 01:42:37 -0600

    add message handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Discord from 'discord.js';
 import config from './config/index.js';
 import commands from './discord/commands/index.js';
+import { COMMAND_PREFIX } from './config/index.js';
 
 config.init();
 
@@ -21,6 +22,28 @@ client.on('error', error => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Add message handler
+client.on('messageCreate', async message => {
+    // Ignore messages from bots
+    if (message.author.bot) return;
+
+    // Check if message starts with command prefix
+    if (!message.content.startsWith(COMMAND_PREFIX)) return;
+
+    const args = message.content.slice(COMMAND_PREFIX.length).trim().split(/ +/);
+    const commandName = args[0].toLowerCase();
+
+    const command = client.commands.get(commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error executing that command!');
+    }
 });
 
 const main = async () => {
